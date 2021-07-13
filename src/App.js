57fb0f4fa6d93b54fbf88a567330e77ecd6f9e18
yxtao <yinxiatao@gmail.com>
2021-07-13 13:51:30 -0700

    Fix a bug

--- src/App.js
+++ src/App.js
@@ -3,21 +3,21 @@ import './styles.css';
 import Input from './Input';
 import Thought from'./Thought';
 var thoughts = []
-
+var thoughtId = 0;
 const App = ()=>{
     
     const [thoughtObjList, setThoughtObjList] = useState(thoughts);
 
     const handleCallback =(text) =>{
         let obj = { text: text,
-                    createAt:Date.now(),};
-
+                    createAt:Date.now(),
+                    id: thoughtId};
+        thoughtId ++;
         setThoughtObjList((preList)=>[...preList, obj]);        
     }
     const handleDeleteCallback =(id) =>{
-        setThoughtObjList((preList)=>preList.filter((thought,index)=>index !== id))
+        setThoughtObjList((preList)=>preList.filter((obj)=>obj.id!== id))
     }
-    
     
     useEffect(()=>{
         const timer = setInterval(()=> {
@@ -30,7 +30,7 @@ const App = ()=>{
     <div>
         <div className="intro">Passing your thoughts. Each thought disappers after 15 seconds. </div>
         <Input parentCallback={handleCallback}/>
-        {thoughtObjList.map((thoughtObj,index)=><Thought id={index} text={thoughtObj.text} createAt={thoughtObj.createAt} parentCallback={handleDeleteCallback}/> )}
+        {thoughtObjList.map((thoughtObj)=><Thought id={thoughtObj.id} text={thoughtObj.text} createAt={thoughtObj.createAt} parentCallback={handleDeleteCallback}/> )}
     </div>)
 }
 export default App
--- src/Thought.js
+++ src/Thought.js
@@ -1,16 +1,14 @@
 import React, {useState} from 'react';
 
 const Thought = (props) =>{
-    const [thought, setThought] = useState(props.text);
-    const [ id, setId] = useState(props.id);
    
     const handleDelete =()=>{
-        props.parentCallback(id);
+        props.parentCallback(props.id);
     }
     return(
         <>
         <div className='thought'> 
-        <span className = 'thoughttext'>{thought}  -- { 15- Math.floor((Date.now()-props.createAt)/1000)}s</span> 
+        <span className = 'thoughttext'>{props.text}  -- { 15- Math.floor((Date.now()-props.createAt)/1000)}s</span> 
         <button onClick={handleDelete}> x </button>
         </div>
         </>
